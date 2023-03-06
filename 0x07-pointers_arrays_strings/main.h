bcbb5a78d1c56a427d2e7097e3d2a6f9805bbbc8
mena-rizkalla <mena.rizkalla1@gmail.com>
2023-03-06 05:49:13 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * File: ,main.h
- * Auth: Firdaus
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
 
-#include <stddef.h>
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
 
-#endif
+	return (s);
+}
