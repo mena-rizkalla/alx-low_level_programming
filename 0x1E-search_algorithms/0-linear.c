2cbddd0cbb2961a8903395f66f073a7addcd4c26
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-07 01:27:56 -0800

    putty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
  * linear_search - find value in array
  *
@@ -9,17 +10,18 @@
  * Return:return the first index where value is located
  */
 
-int linear_search(int *array , size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
-	for (size_t i =0; i<size; i++)
-	{	
-		printf("Value checked array[%ld] = [%d]\n" , i , array[i]);
-		if(array[i] == value)
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
 	}
-
 	return (-1);
 }
